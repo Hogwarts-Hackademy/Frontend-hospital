367068e5943f6efbc7c358403e1cb4f428359eaa
jainankit08 <ankitd0811@gmail.com>
2024-09-01 00:24:33 +0530

    deshBoard css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import Login from "./pages/loginPage.jsx";
-import Dashboard from "./pages/admin/adminDashboard.jsx";
-import AddStaff from "./pages/admin/addStaff/addStaff.jsx";
+	import React from "react";
+	import { Route, Routes, Navigate } from "react-router-dom";
+	import './App.css'; 
+	import Login from "./pages/loginPage.jsx";
+	import Dashboard from "./pages/admin/adminDashboard.jsx";
+	import AddStaff from "./pages/admin/addStaff/addStaff.jsx";
 
-function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Navigate to="/login" />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/admin" element={<Dashboard />} />
-				<Route path="/add-staff" element={<AddStaff />} />
-			</Routes>
-		</div>
-	);
-}
+	function App() {
+		return (
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Navigate to="/login" />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/admin" element={<Dashboard />} />
+					<Route path="/add-staff" element={<AddStaff />} />
+				</Routes>
+			</div>
+		);
+	}
 
-export default App;
+	export default App;
--- src/pages/admin/adminDashboard.jsx
+++ src/pages/admin/adminDashboard.jsx
@@ -1,7 +1,7 @@
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'; // Import Notification icon
 import { Card } from '@mui/material';
 import Sidebar from '../../Components/Sidebar';
-import dashboardImage from '../../assets/images/dashboard.png';
+import dashboardImage from '../../assets/images/dashboard.svg';
 import './adminDashboard.css';
 
 const Dashboard = () => {
@@ -26,49 +26,40 @@ const Dashboard = () => {
                             <div className="call-details">
                                 <p>Ambulance: 24</p>
                                 <p>Patient: 24</p>
-                                <img src={dashboardImage} alt="Emergency Calls Icon" className="card-icon" /> {/* Image added here */}
+                                {/* <img src={dashboardImage} alt="Emergency Calls Icon" className="card-icon" /> Image added here */}
                             </div>
                         </div>
                         <div className="card patient-admission">
                             <h3>Patient Admission</h3>
                             <p className="admission-count">1,287</p>
-                            <div className="admission-graph">
-                                {/* Graph will go here */}
-                            </div>
-                        </div>
+                       </div>
                         <div className="card bed-availability">
                             <h3>Bed Availability</h3>
-                            <Card>
-                                <p>Beds: 24</p>
-                                <p>ICU: 24</p>
-                                <p>Emergency rooms: 24</p>
-                                <p>OT: 24</p>
-                                <p>Ventilators: 24</p>
-                            </Card>
+                            <p>Beds: 24</p>
+                            <p>ICU: 24</p>
+                            <p>Emergency rooms: 24</p>
+                            <p>OT: 24</p>
+                            <p>Ventilators: 24</p>
                         </div>
                         <div className="card staff-availability">
                             <h3>Staff Availability</h3>
-                            <Card>
-                                <p>Doctors: 24</p>
-                                <p>Nurses: 24</p>
-                                <p>Lab technicians: 24</p>
-                                <p>Receptionists: 24</p>
-                                <p>Janitors: 24</p>
-                            </Card>
+                            <p>Doctors: 24</p>
+                            <p>Nurses: 24</p>
+                            <p>Lab technicians: 24</p>
+                            <p>Receptionists: 24</p>
+                            <p>Janitors: 24</p>
                         </div>
                         <div className="card inventory">
                             <h3>Inventory</h3>
-                            <Card>
-                                <p>Out of stock medicines: 24</p>
-                                <p>Ordered medicines: 24</p>
-                                <p>Expired meds: 24</p>
-                            </Card>
+                            <p>Out of stock medicines: 24</p>
+                            <p>Ordered medicines: 24</p>
+                            <p>Expired meds: 24</p>
                         </div>
                         <div className="card other">
                             <h3>Others</h3>
-                            <Card>
-                                <p>Departments: 24</p>
-                            </Card>
+
+                            <p>Departments: 24</p>
+
                         </div>
                     </section>
                 </main>
