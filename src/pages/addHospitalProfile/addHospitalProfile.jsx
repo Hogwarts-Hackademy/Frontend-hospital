40f073fe2af4e3127b150b13be0ff248281170c4
akshatsaini08 <akshatsaini336@gmail.com>
2024-09-08 16:05:17 +0530

    hospital page completed

--- src/pages/addHospitalProfile/addHospitalProfile.jsx
+++ src/pages/addHospitalProfile/addHospitalProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./addHospitalProfile.css";
 import Sidebar from "../../Components/Sidebar";
 
+
 const AddHospitalProfile = () => {
     const [step, setStep] = useState(1);
 
@@ -9,44 +10,46 @@ const AddHospitalProfile = () => {
         e.preventDefault();
         setStep(2); // Move to the next step
     };
-
+    
     return (
         <div className="hospital-profile-container">
             <Sidebar />
+            <div className="card">
+            <h2>Hospital Detail's</h2><div/>
             <div className="main-content">
-                <div className="card">
-                    <h2>Hospital Profile</h2>
+              
                     <form>
-                        <div className="input-group">
+                        <div className="input-group-hospitalname">
                             <label>Hospital Name</label>
                             <input type="text"/>
                         </div>
-                        <div className="input-group">
+                        <div className="input-group-address">
                             <label>Address</label>
                             <input type="text"/>
                         </div>
-                        <div className="input-group">
+                        <div className="input-group-contact">
                             <label>Contact No.</label>
                             <input type="text"/>
                         </div>
-                        <div className="input-group">
+                        <div className="input-group-date">
                             <label>Established Date</label>
                             <input type="email"/>
                         </div>
-                        <div className="input-group">
-                            <label>Specialty</label>
+                        <div className="input-group-owndetail">
+                            <label>Ownership Detail</label>
                             <input type="text"/>
                         </div>
-                        <div className="input-group">
-                            <label></label>
+                        <div className="input-group-aff">
+                            <label>Affiliations</label>
                             <input type="number" />
                         </div>
-                        <div className="input-group">
-                            <label>Ownership Detail</label>
+                        <div className="input-group-hrs">
+                            <label>Visiting Hours</label>
                             <input type="url"/>
                         </div>
+                      
 
-                        <button onClick={handleNext}>Next Step</button>
+                        <button onClick={handleNext}>Next</button>
                     </form>
                 </div>
             </div>
