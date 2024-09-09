954f9812af3ccfd40dc9d5584a9870a831f3531e
akshatsaini08 <akshatsaini336@gmail.com>
2024-09-09 11:05:33 +0530

    nurse notes

--- src/pages/nurseNotes/nurseNotes.jsx
+++ src/pages/nurseNotes/nurseNotes.jsx
@@ -4,36 +4,40 @@ const NurseNotes = () => {
     return (
         <div className="nurse-notes-container">
             <div className="header">
-                <h1>Nurse Notes</h1>
+                <h1>Junior Doctor Notes</h1>
             </div>
             <div className="notes-grid">
-                <div className="patient-info">
+                <div className="patient-info-n">
                     <p><strong>Name:</strong> Abc</p>
                     <p><strong>Age:</strong> 13</p>
                     <p><strong>Gender:</strong> Male</p>
-                    <p><strong>Blood Group:</strong> A+</p>
+                  
                 </div>
-                <div className="date-time-info">
+                <div className="date-time-info-a">
                     <p><strong>Date:</strong> 12/12/2022</p>
                     <p><strong>Time:</strong> 0123456789</p>
+                    <p><strong>Blood Group:</strong> A+</p>
                 </div>
-                <div className="notes-section">
+                <div className="notes-section-b">
                     <h3>Notes:</h3>
                     <div className="scrollable">
                         <p><strong>Observations:</strong></p>
                         {/* <p>/* Add observation details here </p> */}
-                        <p><strong>Care provided:</strong></p>
                         {/* <p>/* Add care details here </p> */}
                     </div>
+                    <div className="notes-section-e">
+                    <p><strong>Care provided:</strong></p>
+
+                    </div>
                 </div>
-                <div className="treatment-updates">
+                <div className="treatment-updates-c">
                     <h3>Treatment Updates:</h3>
                     <div className="scrollable">
                         {/* <p>/* Add treatment updates here </p> */}
                     </div>
                 </div>
             </div>
-            <div className="submit-button">
+            <div className="submit-button-d">
                 <button>Submit</button>
             </div>
         </div>
