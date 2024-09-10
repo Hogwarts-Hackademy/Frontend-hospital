3aa1748f9d2bced71166e822e38a21a341e55110
akshatsaini08 <akshatsaini336@gmail.com>
2024-09-10 18:34:26 +0530

    department info

--- src/pages/departmentInfo/departmentInfo.jsx
+++ src/pages/departmentInfo/departmentInfo.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import "./departmentInfo.css"; // Import the CSS file
 import Sidebar from "../../Components/Sidebar";
+
 const DepartmentInfo = () => {
     return (
         <div className="dashboard-container">
             <Sidebar/>
-            <main className="main-content">
+            <h1 id="h">Department Info</h1>
+            <div className="main-content1">
                 <header className="main-header">
-                    <h1>Department Info</h1>
+                    <label><h2>Doctor</h2></label>
                     <input type="text" placeholder="Search" className="search-input" />
                 </header>
                 <div className="department-cards-container">
                     {[...Array(6)].map((_, index) => (
                         <div key={index} className="department-card">
-                            <h3>Department</h3>
+                            <h3 style={{marginTop:"5px"}}>Department</h3>
                             <p>Cardiology</p>
-                            <p>Doctors: 30</p>
-                            <p>Available: 10</p>
-                            <p>Busy: 27</p>
+                            <div className="card-content">
+                                <p><span>Doctors</span> : 30</p>
+                                <p><span>Available</span> : 10</p>
+                                <p><span>Busy</span> : 27</p>
+                            </div>
                         </div>
                     ))}
                 </div>
-            </main>
+            </div>
         </div>
     );
 };
