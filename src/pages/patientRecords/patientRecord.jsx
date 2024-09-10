d52cdd6da2e177a0af4689889510ef699b29a2f6
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-09-10 10:05:23 +0530

    updated css implementation with change in backend

--- src/pages/patientRecords/patientRecord.jsx
+++ src/pages/patientRecords/patientRecord.jsx
@@ -58,8 +58,8 @@ const PatientRecords = () => {
                             </tr>
                         </thead>
                         <tbody>
-                        {patientRecords.map((patient) => (
-                                <tr key={patient.patientID} className={patient % 2 === 0 ? 'even-row' : 'odd-row'}>
+                        {patientRecords.map((patient, index) => (
+                                <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
                                     <td>{patient.fullName}</td>
                                     <td>{new Date().getFullYear() - new Date(patient.dateOfBirth).getFullYear()}</td>
                                     <td>{patient.contactInformation.address}</td>
