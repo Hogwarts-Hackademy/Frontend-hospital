915c8cf0bad3123c9e862057ebd1324da3f4faca
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-09-11 17:57:02 +0530

    Refactor loginAPI URLs to use staff-user endpoints

--- src/config/loginAPI.jsx
+++ src/config/loginAPI.jsx
@@ -1,9 +1,9 @@
 import BaseURL from "./baseURL";
 
 const loginAPI = {
-	LOGIN_URL: `${BaseURL.LOCAL_BASE_URL}/user/login`,
-	REGISTER_URL: `${BaseURL.LOCAL_BASE_URL}/user/register`,
-	LOGOUT_URL: `${BaseURL.LOCAL_BASE_URL}/user/logout`,
+	LOGIN_URL: `${BaseURL.LOCAL_BASE_URL}/staff-user/login`,
+	REGISTER_URL: `${BaseURL.LOCAL_BASE_URL}/staff-user/register`,
+	LOGOUT_URL: `${BaseURL.LOCAL_BASE_URL}/staff-user/logout`,
 };
 
 export default loginAPI;
