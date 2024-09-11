d929ef634da89b4968d4d1e2ed3df28e7fbb2e63
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-09-11 16:21:36 +0530

    Add baseURL configuration and update API URLs

---
+++ src/config/baseURL.jsx
@@ -0,0 +1,5 @@
+const BaseURL = {
+    LOCAL_BASE_URL: "http://localhost:5000/api"
+};
+
+export default BaseURL;
--- src/config/patientAPI.jsx
+++ src/config/patientAPI.jsx
@@ -1,7 +1,10 @@
+import BaseURL from "./baseURL";
+
 const patientAPI = {
-    FETCH_ALL_PATIENTS_URL: "http://localhost:5000/api/patient/fetchall",
-    FETCH_SINGLE_PATIENT_URL: "http://localhost:5000/api/patient/fetchone",
-    VISIT_HISTORY_URL: "http://localhost:5000/api/patient/get-visithistory",
+	REGISTER_NEW_PATIENT_URL: `${BaseURL.LOCAL_BASE_URL}/patient/register`,
+	FETCH_ALL_PATIENTS_URL: `${BaseURL.LOCAL_BASE_URL}/patient/fetchall`,
+	FETCH_SINGLE_PATIENT_URL: `${BaseURL.LOCAL_BASE_URL}/patient/fetchone`,
+	VISIT_HISTORY_URL: `${BaseURL.LOCAL_BASE_URL}/patient/get-visithistory`,
 };
 
 export default patientAPI;
