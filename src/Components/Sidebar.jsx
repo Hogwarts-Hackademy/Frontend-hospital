03bb334a4ecb22fdbddb4a31ad3cb320d0fb2d2a
jayesh2472 <divyeshjoshi7474@gmail.com>
2024-09-12 11:46:42 +0530

    inventory page update

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -12,10 +12,10 @@ const Sidebar = () => {
       <ul className="sidebar-menu">
         <li><Link className='sidebar-link-text' to="/admin">Dashboard</Link></li>
         <li><Link className='sidebar-link-text' to="/patient-records">Patient records</Link></li>
-        <li><Link className='sidebar-link-text' to="/add-schedules">Add schedules</Link></li>
+        <li><Link className='sidebar-link-text' to="/register-patient">Register patient</Link></li>
         <li><Link className='sidebar-link-text' to="/add-staff">Add staff</Link></li>
-        <li><Link className='sidebar-link-text' to="/department-records">Department records</Link></li>
-        <li><Link className='sidebar-link-text' to="/manage-inventory">Manage Inventory</Link></li>
+        <li><Link className='sidebar-link-text' to="/department-info">Department records</Link></li>
+        <li><Link className='sidebar-link-text' to="/inventory">Manage Inventory</Link></li>
         <li><Link className='sidebar-link-text' to="/hospital-profile">Hospital Profile</Link></li>
       </ul>
       <div className="sidebar-footer">
